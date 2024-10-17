b6c72656f1232c7c6b1bc863306b254586cc9160
vredinn <88535297+vredinn@users.noreply.github.com>
2024-10-17 14:24:11 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,3 +18,37 @@ for (let anchor of anchors) {
     })
   })
 }
+
+// Кнопка вызова формы обратной связи и сама форма
+const orderBtns = document.querySelectorAll(".button_order");
+const modal = document.querySelector(".modal");
+const modalForm = document.querySelector(".modal__form");
+const cancelBtn = document.getElementById("order-cancel");
+const submitBtn = document.getElementById("order-submit");
+
+//Отображение/скрытие модального окна
+const toggleModalVisibility = () => {
+  modal.classList.toggle("modal_active");
+  modalForm.classList.toggle("modal__form_active");
+};
+
+// Закрытие модального окна при клике вне формы
+modal.addEventListener('mousedown', (event) => {
+  if (!modalForm.contains(event.target)) {
+    toggleModalVisibility();
+  }
+});
+//Отображение/скрытие модального окна по нажатию на кнопки
+orderBtns.forEach(btn => {
+  btn.addEventListener('mousedown', () => {
+    toggleModalVisibility();
+  });
+});
+
+submitBtn.addEventListener('mousedown', () => {
+  toggleModalVisibility();
+});
+
+cancelBtn.addEventListener('mousedown', () => {
+  toggleModalVisibility();
+});
