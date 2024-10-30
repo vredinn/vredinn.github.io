52df423693e9d5c3fd6cb061d51c2258a812562e
Victor Dmitriev <fox3un@gmail.com>
2024-10-31 01:39:23 +0500

    adaptation

--- js/script.js
+++ js/script.js
@@ -34,12 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  
-  const main = document.getElementById('main');
+  // Контейнер главного раздела
   const containerMain = document.querySelector('.container_main');
   const waveMain = document.querySelector('.wave_main');
+  // Вычисление высоты главного раздела
   containerMain.style.height = 'calc(100% - ' + waveMain.clientHeight*2 + 'px)';
-
+  // Изменение высоты главного раздела при изменении размера окна
   window.addEventListener('resize', () =>{    
     containerMain.style.height = 'calc(100% - ' + waveMain.clientHeight*2 + 'px)';
   });
